3b41c3799db98447ca2e625da122b4bd2d2362c2
Benhap1 <bahtiyar.bobokulov1@gmail.com>
2024-03-31 18:54:44 +0300

    Revert "PackTest adjustment"
    This reverts commit e04ccd43e37c82b56c8f12c0f86919d23ea7b800.

--- domain/src/test/java/ru/fastdelivery/domain/delivery/shipment/ShipmentTest.java
+++ domain/src/test/java/ru/fastdelivery/domain/delivery/shipment/ShipmentTest.java
@@ -1,33 +1,27 @@
-package ru.fastdelivery.domain.delivery.shipment;
-
-import org.junit.jupiter.api.Test;
-import ru.fastdelivery.domain.common.currency.CurrencyFactory;
-import ru.fastdelivery.domain.common.volume.Volume;
-import ru.fastdelivery.domain.common.weight.Weight;
-import ru.fastdelivery.domain.delivery.pack.Pack;
-
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-class ShipmentTest {
-
-    @Test
-    void whenSummarizingWeightOfAllPackages_thenReturnSum() {
-        var weight1 = new Weight(BigDecimal.TEN);
-        var weight2 = new Weight(BigDecimal.ONE);
-
-        var volume1 = new Volume(BigInteger.TEN, BigInteger.TEN, BigInteger.TEN); // Пример объема упаковки 1
-        var volume2 = new Volume(BigInteger.ONE, BigInteger.ONE, BigInteger.ONE); // Пример объема упаковки 2
-
-        var packages = List.of(new Pack(weight1, volume1), new Pack(weight2, volume2)); // Используем объем в создании Pack
-        var shipment = new Shipment(packages, new CurrencyFactory(code -> true).create("RUB"));
-
-        var massOfShipment = shipment.weightAllPackages();
-
-        assertThat(massOfShipment.weightGrams()).isEqualByComparingTo(BigDecimal.valueOf(11));
-    }
-}
-
+//package ru.fastdelivery.domain.delivery.shipment;
+//
+//import org.junit.jupiter.api.Test;
+//import ru.fastdelivery.domain.common.currency.CurrencyFactory;
+//import ru.fastdelivery.domain.common.weight.Weight;
+//import ru.fastdelivery.domain.delivery.pack.Pack;
+//
+//import java.math.BigInteger;
+//import java.util.List;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//class ShipmentTest {
+//
+//    @Test
+//    void whenSummarizingWeightOfAllPackages_thenReturnSum() {
+//        var weight1 = new Weight(BigInteger.TEN);
+//        var weight2 = new Weight(BigInteger.ONE);
+//
+//        var packages = List.of(new Pack(weight1), new Pack(weight2));
+//        var shipment = new Shipment(packages, new CurrencyFactory(code -> true).create("RUB"));
+//
+//        var massOfShipment = shipment.weightAllPackages();
+//
+//        assertThat(massOfShipment.weightGrams()).isEqualByComparingTo(BigInteger.valueOf(11));
+//    }
+//}
