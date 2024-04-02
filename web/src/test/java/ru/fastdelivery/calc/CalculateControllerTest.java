e42acf9dff670ec5f2914ecab4c498c697a5d957
Benhap1 <bahtiyar.bobokulov1@gmail.com>
2024-04-02 12:13:22 +0300

    Test adjustment

--- web/src/test/java/ru/fastdelivery/calc/CalculateControllerTest.java
+++ web/src/test/java/ru/fastdelivery/calc/CalculateControllerTest.java
@@ -1,18 +1,24 @@
 package ru.fastdelivery.calc;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
 import ru.fastdelivery.domain.common.currency.Currency;
 import ru.fastdelivery.domain.common.currency.CurrencyFactory;
 import ru.fastdelivery.domain.common.distance.DistanceCalculator;
+import ru.fastdelivery.domain.common.price.Price;
 import ru.fastdelivery.domain.common.volume.Volume;
 import ru.fastdelivery.domain.common.weight.Weight;
 import ru.fastdelivery.domain.delivery.pack.Pack;
 import ru.fastdelivery.domain.delivery.shipment.Shipment;
 import ru.fastdelivery.presentation.api.request.CalculatePackagesRequest;
+import ru.fastdelivery.presentation.api.response.CalculatePackagesResponse;
 import ru.fastdelivery.presentation.calc.CalculateController;
 import ru.fastdelivery.usecase.TariffCalculateUseCase;
 
@@ -20,7 +26,8 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
 
-@ExtendWith(MockitoExtension.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CalculateControllerTest {
 
     @Mock
@@ -34,6 +41,11 @@ class CalculateControllerTest {
 
     @InjectMocks
     private CalculateController calculateController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void testCalculatePackages() {
@@ -49,5 +61,17 @@ class CalculateControllerTest {
         List<Pack> packs = List.of(new Pack(new Weight(BigDecimal.valueOf(4564)), new Volume(BigInteger.valueOf(345), BigInteger.valueOf(589), BigInteger.valueOf(234))));
         Shipment expectedShipment = new Shipment(packs, currency);
 
+        when(currencyFactory.create(anyString())).thenReturn(new Currency("RUB"));
+        when(distanceCalculator.calculateDistance(anyDouble(), anyDouble(), anyDouble(), anyDouble())).thenReturn(1234.56);
+        when(tariffCalculateUseCase.minimalPrice()).thenReturn(new Price(BigDecimal.valueOf(350), new Currency("RUB")));
+        when(tariffCalculateUseCase.calc(eq(expectedShipment),anyDouble())).thenReturn(new Price(BigDecimal.valueOf(7302.4), new Currency("RUB")));
+
+        // Act
+        CalculatePackagesResponse response = calculateController.calculate(request);
+
+        // Assert
+        assertEquals(new BigDecimal("7302.4"),response.totalPrice());
+        assertEquals(new BigDecimal("350"), response.minimalPrice());
+        assertEquals("RUB", response.currencyCode());
     }
 }
