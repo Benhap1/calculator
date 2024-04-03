6d485d35b6c0bbe1746842fda23a8ad00592a3b2
Benhap1 <bahtiyar.bobokulov1@gmail.com>
2024-04-03 16:09:04 +0300

    Request body adjustment

--- web/src/main/java/ru/fastdelivery/presentation/api/request/CalculatePackagesRequest.java
+++ web/src/main/java/ru/fastdelivery/presentation/api/request/CalculatePackagesRequest.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Schema(description = "Данные для расчета стоимости доставки")
 public record CalculatePackagesRequest(
         @Schema(description = "Список упаковок отправления",
-                example = "[{\"weight\": 4056.45}]")
+                example = "[{\"weight\": 4056.45,\"length\": 345,\"width\": 589,\"height\": 234} ]")
         @NotNull
         @NotEmpty
         List<CargoPackage> packages,
@@ -18,10 +18,13 @@ public record CalculatePackagesRequest(
         @NotNull
         String currencyCode,
 
-        @Schema(description = "Координаты пункта назначения")
+        @Schema(description = "Координаты пункта назначения",
+                example = "{\"latitude\": 45,\"longitude\": 80}")
         @NotNull Coordinate destination,
 
-        @Schema(description = "Координаты пункта отправления")
+        @Schema(description = "Координаты пункта отправления",
+                example = "{\"latitude\": 48,\"longitude\": 85}")
+
         @NotNull
         Coordinate departure
 ) {
